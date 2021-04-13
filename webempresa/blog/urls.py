0359de8ef5b4387a0c4fb7500d8854d0b88d28cb
Wilmer Santiago Ramos <santi050699@gmail.com>
2021-04-13 16:39:15 -0500

    Añadido Proyecto

---
+++ webempresa/blog/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+from .import views
+
+urlpatterns = [
+
+    #Paths del Blog
+
+    path('', views.blog, name='blog'),
+    path('category/<int:category_id>', views.category, name="category"),
+
+
+]
